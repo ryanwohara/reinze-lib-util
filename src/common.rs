63e4d61d7096d149e8010ade51ab468f3789b01e
Ryan William O'Hara <ryan@ryanwohara.com>
2023-03-19 20:31:26 -0600

    fix: Support calculating larger numbers

--- src/common.rs
+++ src/common.rs
@@ -55,7 +55,7 @@ pub fn p(s: &str) -> String {
 
 // Adds commas to a string
 pub fn commas_from_string(n: &str) -> String {
-    let n = match n.parse::<f32>() {
+    let n = match n.parse::<f64>() {
         Ok(n) => n,
         Err(_) => 0.0,
     };
