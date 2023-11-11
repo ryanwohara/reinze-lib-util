fbac0d44f9b4ec1037c090571c667467cd552fa1
Ryan William O'Hara <ryan@ryanwohara.com>
2023-11-10 20:16:17 -0700

    refactor: Calc

--- src/calculator.rs
+++ src/calculator.rs
@@ -1,52 +1,50 @@
 extern crate common;
-
 use meval::eval_str;
 use regex::Regex;
 
 pub fn calculate(query: &str) -> Result<Vec<String>, ()> {
     // From iKick's source
     // alias calc2 return $calc($regsubex($remove($1-,$chr(44)),/([\d.]+)([kmb])/gi,$calc(\1 * $+(1,$str(000,$pos(kmb,\2,1))))))
-
     let re_verify = Regex::new(r"^(?:pi|e|abs|sqrt|exp|ln|sin|cos|tan|asin|acos|atan|atan2|sinh|cosh|tanh|asinh|acosh|atanh|floor|ceil|round|[-+%/*^\d\s()><=,.kmb])+$").unwrap();
 
     if !re_verify.is_match(query) {
         return Err(());
     }
 
-    let no_commas = query.replace(",", "");
+    let result = eval_query(query.replace(",", ""))?;
 
+    Ok(vec![format!(
+        "{} {} = {}",
+        common::l("Calc"),
+        common::c1(query),
+        common::c2(&common::commas(result, "f"))
+    )])
+}
+
+fn eval_query(query: String) -> Result<f64, ()> {
     let re_kmb = Regex::new(r"(?P<num>[\d.]+)(?P<kmb>[kmb])").unwrap();
     let processed = re_kmb
-        .replace_all(&no_commas, |caps: &regex::Captures| {
-            let num = caps.name("num").unwrap().as_str();
-            let kmb = caps.name("kmb").unwrap().as_str();
-
+        .replace_all(&query, |caps: &regex::Captures| {
+            let (num, kmb) = (
+                caps.name("num").unwrap().as_str(),
+                caps.name("kmb").unwrap().as_str(),
+            );
             let mut num = num.parse::<f64>().unwrap_or_default();
 
-            match kmb {
-                "k" => num *= 1000.0,
-                "m" => num *= 1000000.0,
-                "b" => num *= 1000000000.0,
-                _ => (),
+            if let Some(factor) = match kmb {
+                "k" => Some(1_000.0),
+                "m" => Some(1_000_000.0),
+                "b" => Some(1_000_000_000.0),
+                _ => None,
+            } {
+                num *= factor;
             }
-
             num.to_string()
         })
         .to_string();
 
-    let result = match eval_str(processed) {
-        Ok(f) => f,
-        Err(e) => {
-            println!("Error: {}", e);
-            return Err(());
-        }
-    };
-
-    Ok(vec![format!(
-        "{} {} {} {}",
-        common::l("Calc"),
-        common::c1(query),
-        common::c1("="),
-        common::c2(&common::commas(result, "f"))
-    )])
+    eval_str(&processed).map_err(|e| {
+        println!("Error: {}", e);
+        ()
+    })
 }
