e85b5048e4efbc3f77dc763ecfbab57e54b9c9d6
Ryan William O'Hara <ryan@ryanwohara.com>
2025-10-01 12:45:32 -0600

    chore: cargo update in deployment

--- bin/bd-prod.sh
+++ bin/bd-prod.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 # bd = build & deploy
 
-cargo build --release && rm -rf ../rust-reinze/plugins/libreinze_lib_util_*.so && cp target/release/libreinze_lib_util.so ../rust-reinze/plugins/libreinze_lib_util_$(date "+%Y%m%dT%H%M%S").so
+cargo update && \
+  cargo build --release && \
+  rm -rf ../rust-reinze/plugins/libreinze_lib_util_*.so && \
+  cp target/release/libreinze_lib_util.so ../rust-reinze/plugins/libreinze_lib_util_$(date "+%Y%m%dT%H%M%S").so
