f7609dacfe2a5df345db1f9a7ae8782c1b2eaa9c
Ryan William O'Hara <ryan@ryanwohara.com>
2025-10-01 12:55:44 -0600

    feat(ci): Multiplatform support

--- bin/bd-prod.sh
+++ bin/bd-prod.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 # bd = build & deploy
 
+[[ $(uname -s) == "Darwin" ]] && export extension=.dylib || export extension=.so
+
 cargo update && \
   cargo build --release && \
-  rm -rf ../rust-reinze/plugins/libreinze_lib_util_*.so && \
-  cp target/release/libreinze_lib_util.so ../rust-reinze/plugins/libreinze_lib_util_$(date "+%Y%m%dT%H%M%S").so
+  rm -rf ../rust-reinze/plugins/libreinze_lib_util_* && \
+  cp "target/release/libreinze_lib_util${extension}" "../rust-reinze/plugins/libreinze_lib_util_$(date "+%Y%m%dT%H%M%S")${extension}"
